1c0a9dcf1c75e4552e1e8a837116d57c132e5429
Israel Cena <israelsjm@gmail.com>
2022-05-24 21:31:45 -0300

    first commit

---
+++ public/exercicio1.php
@@ -0,0 +1,14 @@
+<?php
+echo "<h1>Exercício de Algoritmo 1 </h1>";
+echo "<h2>Como trocar um pneu furado ?</h2>";
+echo "<ol>";
+echo "<li>Se estiver dentro do carro, Sair do carro;</li>";
+echo "<li>Sinalizar na Rua;</li>";
+echo "<li>Afrouxar levemente os parafusos do pneu furado;</li>";
+echo "<li>Subir o carro com o Macaco;</li>";
+echo "<li>Retirar o pneu furado do carro;</li>";
+echo "<li>Retirar o pneu estepe do local de armazenamento;</li>";
+echo "<li>Colocar o pneu estepe no lugar do pneu furado;</li>";
+echo "<li>Guardar o pneu furado para reparo futuro.</li>";
+echo "</ol>";
+echo "<button onclick=\"window.location.href = './index.php';\">Voltar</button>";
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>Exercícios de Algoritmo </h1>";
+
+echo "<ol>";
+echo "<li><a href=\"./exercicio1.php\">Exercício 1 - Como trocar um pneu furado ?</a></li>";
+echo "</ol>";
