8bcc67e6283aea2cb738fbb85c64824c7caf9dba
Israel Cena <israelsjm@gmail.com>
2022-05-27 11:13:24 -0300

    add exercicio12

--- public/exercicio12.php
+++ public/exercicio12.php
@@ -1,14 +1,19 @@
 <form action="" method="post">
-	<label for="operacao">Selecione a operação: </label>
-	<select name="operacao" id="operacao">
-		<option value="deposito">Deposito</option>
-		<option value="saque">Saque</option>
-	</select>
+	<label for="value1">Digite um número: </label>
+	<input type="number" name="value1" id="value1" required ><br />
+	<label for="value2">Digite outro número: </label>
+	<input type="number" name="value2" id="value2" required ><br />
 	<button>Confirmar</button>
 </form>
 
 <?php
 	if ($_POST) {
+		echo "Os múltiplos de 3 entre {$_POST['value1']} e {$_POST['value2']} são: ";
+		for ($i = $_POST['value1']; $i <= $_POST['value2']; $i++) {
+			if ($i % 3 == 0) {
+				echo $i . " ";
+			}
+		}
 	}
 
 echo "<br/>";
