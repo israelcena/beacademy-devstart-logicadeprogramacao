cc89f6355bfe8e4eae0b5e593f61b91ba3996022
Israel Cena <israelsjm@gmail.com>
2022-05-27 12:42:15 -0300

    include exercicio15

--- public/exercicio15.php
+++ public/exercicio15.php
@@ -1,14 +1,23 @@
+<h1>Login e Senha</h1>
 <form action="" method="post">
-	<label for="operacao">Selecione a operação: </label>
-	<select name="operacao" id="operacao">
-		<option value="deposito">Deposito</option>
-		<option value="saque">Saque</option>
-	</select>
+	<label for="nome">Digite seu nome:</label>
+	<input type="text" name="nome" id="nome" required /><br />
+		<label for="password">Digite sua senha:</label>
+	<input type="password" name="password" id="password" required /><br />
 	<button>Confirmar</button>
 </form>
 
 <?php
 	if ($_POST) {
+		$i = 0;
+		while (($_POST["nome"] != "admin") && ($_POST["password"] != "admin") && $i < 1) {
+			$i++;
+			echo "Usuário ou senha inválidos! <br />";
+		}
+		if ($_POST["nome"] == "admin" && $_POST["password"] == "admin") {
+			echo "Bem vindo, " . $_POST["nome"] . "! <br />";
+		}
+		
 	}
 
 echo "<br/>";
